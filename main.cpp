f81583299d12e91f98151e9dbaee71ec1e8da4ff
PetterJ123 <mr.pepe999@gmail.com>
2018-11-14 15:51:49 +0100

    Program to do average

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-
+#include <string>
 /**
  * The program reads numbers from a file and prints the sum of them
  * Example: main.exe ./tests/input.in
@@ -8,26 +8,46 @@
  * @param  argv [description]
  * @return      [description]
  */
-int main(int argc, const char * argv[])
-{
-    
-    int a = 0;
-    int sum = 0;
-    
-    std::ifstream myReadFile;
-    
-    myReadFile.open(argv[1]);
-    
-    while (myReadFile >> a)
-    {
-        sum += a;
-    }
-    myReadFile.close();
-    
-    
-    
-    std::cout << sum << "\n";
-    
-    return 0;
-}
+int main(int argc, const char * argv[]){
+	std::ifstream readFile;
 
+	readFile.open("./tests/input.in");	// Opens file in read-mode
+
+	if(!readFile){						// Checks if program can't read file
+		std::cout << "Can't open text-file\n";
+		exit(EXIT_FAILURE);
+	}
+
+	int count;
+	int i=0;
+	int readInt;
+	while(readFile >> readInt){		// Counts the number of integers
+		count++;
+		i++;
+	}
+
+	int* arr = new int[count];
+
+	double sum;
+	double average;
+
+	readFile.clear();
+	readFile.seekg(0, std::ios::beg);
+	for (int i=0; i<count; i++) {
+		readFile >> arr[i];
+		sum = sum + arr[i];
+	}
+	average = sum / count;
+	std::cout << "Sum is: " << sum << "\n";
+	std::cout << "Average is: " << average << "\n";
+
+	for(int i=0; i<count; i++){
+		if(average < arr[i]){
+			std::cout << arr[i] << " is greater than " << average << "\n";
+		}
+	}
+
+	delete [] arr;
+
+	return 0;
+}
